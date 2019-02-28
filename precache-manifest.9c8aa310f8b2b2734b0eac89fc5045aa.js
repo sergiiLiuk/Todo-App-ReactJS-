74edf3ebd395ad5bf83f0d7ece510f98fbcf5d71
sergii <sergiiliuk@yahoo.dk>
2019-02-28 18:56:59 +0100

    Updates

---
+++ precache-manifest.9c8aa310f8b2b2734b0eac89fc5045aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7729106121ddc1d1b0a1",
+    "url": "/sergiiLiuk/Todo-App-ReactJS-/static/js/runtime~main.77291061.js"
+  },
+  {
+    "revision": "4791a4e1ad294a5e6efc",
+    "url": "/sergiiLiuk/Todo-App-ReactJS-/static/js/main.4791a4e1.chunk.js"
+  },
+  {
+    "revision": "89acd34e6615b52721ee",
+    "url": "/sergiiLiuk/Todo-App-ReactJS-/static/js/2.89acd34e.chunk.js"
+  },
+  {
+    "revision": "4791a4e1ad294a5e6efc",
+    "url": "/sergiiLiuk/Todo-App-ReactJS-/static/css/main.7cb66479.chunk.css"
+  },
+  {
+    "revision": "219594ef91e4845fe4d2e92a0100bb6d",
+    "url": "/sergiiLiuk/Todo-App-ReactJS-/index.html"
+  }
+];
